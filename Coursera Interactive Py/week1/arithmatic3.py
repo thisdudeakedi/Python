2020050e3a0319e803ef02fd72db3bef2c449495
Max Musau <maxmusau>
2016-05-13 01:28:36 +0300

    CE-Q3

---
+++ Coursera Interactive Py/week1/arithmatic3.py
@@ -0,0 +1,10 @@
+print ('Rectangle Perimeter Calculator 2W+2L')
+print ('Enter Length of 1st side')
+w=raw_input()
+w=int(w)
+print ('Enter Enter Length of 2nd Side')
+y=raw_input()
+y=int(y)
+p=(w+w)+(y+y)
+
+print ('Hello, a rectangle of side'), w, ('and side'), y, ('has a perimeter of = '), p
