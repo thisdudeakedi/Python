63dfab7a71534255f2ec4f8769524a5bd561a945
techmusau <maxmusau@gmail.com>
2016-07-04 07:01:36 +0300

    Signed-off-by: techmusau <maxmusau@gmail.com>

---
+++ Coursera Interactive Py/Week InteractivePy/timer.py
@@ -0,0 +1,19 @@
+#import simplegui
+import time
+from threading import Timer
+
+# Event handler
+def tick():
+    print "tick!"
+
+# Register handler
+#timer = simplegui.create_timer(1000, tick)
+t = 1 #counts down
+while t>0:
+      tick()
+      time.sleep(1) #waits
+      
+#t = Timer(0.6, tick)
+
+# Start timer
+t.start()
